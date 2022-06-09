788f5d0a6d2370e97c8f428ae3ba2ca260dd8ddf
jrodr117 <jrodr113@msudenver.edu>
2022-06-09 14:28:02 -0600

    added a person schema/model

--- app.js
+++ app.js
@@ -18,4 +18,18 @@ const fruit = new Fruit({
 	review: 'solid fruit',
 });
 
-fruit.save();
+// fruit.save();
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	age: Number,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const person = new Person({
+	name: 'John',
+	age: 37,
+});
+
+// person.save();
