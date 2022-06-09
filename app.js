5ea81a00b20529f106a65804be0b8b6add44f83b
jrodr117 <jrodr113@msudenver.edu>
2022-06-09 15:37:46 -0600

    deleted an entry using deleteOne entry

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ mongoose.connect('mongodb://localhost:27017/fruitsDB', {
 });
 
 const fruitSchema = new mongoose.Schema({
-	name: String,
-	rating: Number,
+	name: {
+		type: String,
+		required: [true, 'You need to add a fruit name'],
+	},
+	rating: {
+		type: Number,
+		min: 1,
+		max: 10,
+	},
 	review: String,
 });
 
@@ -17,17 +24,18 @@ const fruit = new Fruit({
 	rating: 7,
 	review: 'solid fruit',
 });
-const banana = new Fruit({
-	name: 'banana',
-	rating: 7,
-	review: 'solid fruit',
-});
 
-const kiwi = new Fruit({
-	name: 'kiwi',
-	rating: 9,
-	review: 'solid fruit',
-});
+// const banana = new Fruit({
+// 	name: 'banana',
+// 	rating: 7,
+// 	review: 'solid fruit',
+// });
+
+// const kiwi = new Fruit({
+// 	name: 'kiwi',
+// 	rating: 9,
+// 	review: 'solid fruit',
+// });
 
 // Fruit.insertMany([banana, kiwi], (err) => {
 // 	if (err) {
@@ -59,8 +67,32 @@ Fruit.find((err, fruits) => {
 		console.log(fruits);
 	}
 
+	// mongoose.connection.close();
+
 	//loops through our fruits array and forEach fruit prints out their name
 	fruits.forEach((fruit) => {
 		console.log(fruit.name);
 	});
 });
+
+//updates an entry thats mising name field
+// Fruit.updateOne(
+// 	{ _id: '62a25587468393f4e9fcfbf5' },
+// 	{ name: 'peach' },
+// 	(err) => {
+// 		if (!err) {
+//             console.log('Update succesful');
+//         } else {
+// 			console.log(err);
+// 		}
+// 	}
+// );
+
+//deletes one entry with the mathing id condition
+// Fruit.deleteOne({_id: '62a25587468393f4e9fcfbf5'}, (err) {
+//     if(!err){
+//         console.log(yes)
+//     } else {
+//         (err)
+//     }
+// })
