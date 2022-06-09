480b34721cf610ad80112ac5265fd38da3151b26
jrodr117 <jrodr113@msudenver.edu>
2022-06-09 14:17:53 -0600

    leveraged mongoose odm to create a new fruitSchema and a fruit model

---
+++ app.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/fruitsDB', {
+	useNewUrlParser: true,
+});
+
+const fruitSchema = new mongoose.Schema({
+	name: String,
+	rating: Number,
+	review: String,
+});
+
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+const fruit = new Fruit({
+	name: 'apple',
+	rating: 7,
+	review: 'solid fruit',
+});
+
+fruit.save();
